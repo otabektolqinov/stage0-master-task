a3b54229e89a90dc851fd8a5c237eaa5f5e61759
Tolqinov Otabek <otabektolqinov30@gmail.com>
2025-09-16 16:13:50 +0500

    LangSyntax tasks completed

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int thousands = number / 1000;
+        int hundreds = (number - thousands * 1000) / 100;
+        int tens = (number - (hundreds * 100 + thousands * 1000)) / 10;
+        int ones = number - (hundreds * 100 + tens * 10 + thousands * 1000);
+        System.out.println(thousands + hundreds + tens + ones);
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int hundreds = number / 100;
+        int tens = (number - hundreds * 100) / 10;
+        int ones = number - (hundreds * 100 + tens * 10);
+
+        number = ones * 100 + tens * 10 + hundreds;
+        System.out.println(number);
     }
 
 }
