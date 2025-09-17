5602728f9111118eff15f45fbc8e97aa49f54693
Tolqinov Otabek <otabektolqinov30@gmail.com>
2025-09-17 15:12:05 +0500

    Conditions tasks completed

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,17 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber == 1 || monthNumber == 2 || monthNumber == 12) {
+            System.out.println("Winter");
+        } else if (monthNumber == 0){
+            System.out.println("Wrong month number");
+        }else if (monthNumber >= 3 && 5>= monthNumber) {
+            System.out.println("Spring");
+        } else if (monthNumber >= 6 && 8 >= monthNumber){
+            System.out.println("Summer");
+        } else if (monthNumber >= 9 && 11 >= monthNumber) {
+            System.out.println("Autumn");
+        }
     }
 
 }
--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,14 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide < secondSide + thirdSide &&
+            secondSide < firstSide + thirdSide &&
+                thirdSide < firstSide + secondSide
+        ){
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
     }
 
 }
